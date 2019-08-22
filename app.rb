8a8a26b059ee921ff491ad4ce0e4030aac2bd973
Jessica Hong <jessicahong.ngnr@gmail.com>
2019-08-22 21:33:52 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -1,9 +1,13 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
-  get '/'do
+  get '/' do
     @session = session
     erb:index
+  end
+  
+  get '/checkout' do
+    erb:checkout
   end
   
   configure do 
